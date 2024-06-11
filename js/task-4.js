374c3f9c120baffa9721f5d8835ca0b4db459ac1
Eugene Soshyn <sichi331@gmail.com>
2024-06-11 18:13:45 +0300

    add 4 task. add gitignore. add code to index.html

--- js/task-4.js
+++ js/task-4.js
@@ -10,10 +10,19 @@ function getShippingCost(country) {
     case "Australia":
       currentCountyShippingPrice = `Shipping to ${country} will cost 170 credits`;
       break;
-    case "Chile":
-      currentCountyShippingPrice = `Shipping to ${country} will cost 250 credits`;
+    case "Jamaica":
+      currentCountyShippingPrice = `Shipping to ${country} will cost 120 credits`;
       break;
     default:
+      currentCountyShippingPrice = `Sorry, there is no delivery to your country`;
       break;
   }
+  return currentCountyShippingPrice;
 }
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
